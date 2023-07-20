cc1fa6f39955cdff76af014dd08bbfb25679ca74
Fyrlex <fyrlex@gmail.com>
2023-07-19 22:27:11 -0400

    push

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,7 @@
+<div class="text-center">
+	<h1 class="text-4xl">Hi, my name is Jaren Goldberg</h1>
+	<p>
+		I am a 18 year old rising freshman and Virginia Tech majoring in Computer
+		Science. I grew up in Fairfax, Virginia.
+	</p>
+</div>
