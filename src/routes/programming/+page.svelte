d5a99c5441d6feef6af2d928d4d8462531a5f6b0
Fyrlex <fyrlex@gmail.com>
2023-07-22 13:13:50 -0400

    changes

--- src/routes/programming/+page.svelte
+++ src/routes/programming/+page.svelte
@@ -1,10 +1,8 @@
-<div class="h-screen bg-stone-900 text-gray-300">
-	<div class="w-1/2 mx-auto">
-		<h1 class="text-8xl text-center font-semibold p-8">
-			programming experience
-		</h1>
+<div class="h-screen bg-stone-900 text-gray-300 font-extralight">
+	<div class="">
+		<h1 class="text-8xl p-8">programming experience</h1>
 		<img
-			class="mx-auto m-5"
+			class="m-5"
 			alt="Skill Icons"
 			width="500"
 			src="https://skillicons.dev/icons?i=nodejs,ts,svelte,html,react,git,github,discord,mongodb,mysql,nginx,cloudflare&theme=dark&perline=6"
