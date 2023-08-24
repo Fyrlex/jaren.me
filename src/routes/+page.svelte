04ffc7c39f868e36c749b2fcce8d9d730e88736f
Jaren Goldberg <fyrlex@gmail.com>
2023-08-24 10:30:14 -0400

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,30 @@
+<script>
+	import Meta from "../components/meta.svelte";
+</script>
+
 <svelte:head>
-	<title>Jaren.me | Home</title>
-			<meta charSet='utf-8' />
-			<meta
-				name='viewport'
-				content='width=device-width, initial-scale=1'
-			/>
-			<meta
-				name='description'
-				content='Hi, I’m Jaren Goldberg. Check out my website to see what I do!'
-			/>
-			<link
-				rel='shortcut icon'
-				href='https://cdn.discordapp.com/avatars/292821168833036288/6e131f90d887abb0b97b1d2c0ec659d2.png?size=4096'
-				type='image/png'
-			/>
-			<meta
-				property='og:title'
-				content='Home'
-			/>
-			<meta
-				property='og:description'
-				content='Hi, I’m Jaren Goldberg. Check out my website to see what I do!'
-			/>
-			<meta
-				property='og:type'
-				content='website'
-			/>
-			<meta
-				property='og:url'
-				content='https://jaren.me'
-			/>
-			<meta
-				property='og:image'
-				content='https://cdn.discordapp.com/avatars/292821168833036288/6e131f90d887abb0b97b1d2c0ec659d2.png?size=4096'
-			/>
-			<meta
-				content='#06B6D4'
-				data-react-helmet='true'
-				name='theme-color'
-			/>
+	<Meta data={{ title: "Home", url: "" }} />
 </svelte:head>
 
 <div class="bg-stone-900 text-center text-gray-300">
-	<h1 class="text-8xl font-semibold p-10">hey!</h1>
-	<p class="text-2xl w-1/2 max-w-fit mx-auto">
-		My name is Jaren Goldberg and I'm from Northern Virginia. Currently I am a
-		rising freshman at Virginia Tech and will be majoring in Computer Science.
+	<h1 class="text-8xl font-semibold p-10">Hey!</h1>
+	<p class="text-2xl w-1/3 max-w-fit mx-auto">
+		My name is Jaren Goldberg. Currently I am a freshman at Virginia Tech and
+		plan to major in Computer Science. I enjoy many activies including sports
+		and the gym, astronomy, playing music with my band, and being outside in
+		nature.
 	</p>
-	<h2 class="text-6xl p-8">A summary</h2>
-	<p class="text-2xl w-1/2 max-w-fit mx-auto">
-		Performing exciting shows with my band, lifting heavy weights in the gym,
-		photographing the night sky, doing the daily chess.com puzzle are all
-		activies I enjoy. In addition to all that, professional programming is my
-		goal (I even built this website).
-	</p>
+	<div class="max-w-fit mx-auto m-5">
+		<a
+			href="https://github.com/Fyrlex"
+			target="_blank"
+			rel="noreferrer noopener"
+		>
+			<img
+				alt="followers"
+				title="Follow me on Github"
+				src="https://img.shields.io/github/followers/Fyrlex?color=236ad3&style=for-the-badge&logo=github&label=Follow"
+			/>
+		</a>
+	</div>
 </div>
--- src/routes/programming/+page.svelte
+++ src/routes/programming/+page.svelte
@@ -1,44 +1,9 @@
+<script>
+	import Meta from "../../components/meta.svelte";
+</script>
+
 <svelte:head>
-	<title>Jaren.me | Programming</title>
-			<meta charSet='utf-8' />
-			<meta
-				name='viewport'
-				content='width=device-width, initial-scale=1'
-			/>
-			<meta
-				name='description'
-				content='Hi, I’m Jaren Goldberg. Check out my website to see what I do!'
-			/>
-			<link
-				rel='shortcut icon'
-				href='https://cdn.discordapp.com/avatars/292821168833036288/6e131f90d887abb0b97b1d2c0ec659d2.png?size=4096'
-				type='image/png'
-			/>
-			<meta
-				property='og:title'
-				content='Programming'
-			/>
-			<meta
-				property='og:description'
-				content='Hi, I’m Jaren Goldberg. Check out my website to see what I do!'
-			/>
-			<meta
-				property='og:type'
-				content='website'
-			/>
-			<meta
-				property='og:url'
-				content='https://jaren.me/programming'
-			/>
-			<meta
-				property='og:image'
-				content='https://cdn.discordapp.com/avatars/292821168833036288/6e131f90d887abb0b97b1d2c0ec659d2.png?size=4096'
-			/>
-			<meta
-				content='#06B6D4'
-				data-react-helmet='true'
-				name='theme-color'
-			/>
+	<Meta data={{ title: "Programming", url: "programming" }} />
 </svelte:head>
 
 <div class="h-screen bg-stone-900 text-gray-300 font-extralight">
