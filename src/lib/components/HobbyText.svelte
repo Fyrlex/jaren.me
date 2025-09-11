e85fe557d85a43484b992816421dd267e0b7c5a9
Jaren Goldberg <fyrlex@gmail.com>
2025-09-11 10:33:24 -0400

    fix: hobby text component description

--- src/lib/components/HobbyText.svelte
+++ src/lib/components/HobbyText.svelte
@@ -4,7 +4,7 @@
   let { date, description, title }: HobbyTextOptions = $props();
 </script>
 
-<div class="relative mx-auto flex flex-col">
+<div class="relative mx-auto flex flex-col justify-between">
   <h3 class="m-2 text-2xl text-cyan-500 md:text-3xl xl:text-4xl">
     {title}
   </h3>
@@ -13,7 +13,7 @@
       {description}
     </p>
   {/if}
-  <p class="absolute inset-x-0 bottom-0 p-2 text-lg lg:text-xl">
+  <p class="inset-x-0 bottom-0 p-2 text-lg lg:text-xl">
     {date}
   </p>
 </div>
